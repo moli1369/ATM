6bb99040861b971ab4db65042592ecfe0a69e004
tayyebi <tayyebimohammadreza@gmail.com>
2017-07-04 18:07:08 +0430

    Added summernote, faltpickr

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -17,16 +17,16 @@ namespace ATM
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.js",
-            //          "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      //"~/Content/bootstrap.css",
-                      // "~/Content/site.css",
+                      "~/Content/bootstrap.css",
+                      //"~/Content/site.css",
                       "~/Content/Style.css"));
         }
     }
