6114e35b69d9353787383d84202fa492ca2e2dd0
tayyebi <tayyebimohammadreza@gmail.com>
2017-07-06 08:17:28 +0430

    Edit Profile

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ATM.Models;
+
+namespace ATM.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private MainModel db = new MainModel();
+
+        // GET: People
+        public ActionResult Index()
+        {
+            return View(db.People.ToList());
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using ATM.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -9,11 +10,11 @@ using System.Web.Mvc;
 
 namespace ATM.Controllers
 {
+    [Gordibute]
     public class UserController : Controller
     {
         public MainModel db = new MainModel();
 
-        [Gordibute]
         public ActionResult MyProfile()
         {
             var id = Guid.Parse(Session["UserId"].ToString());
@@ -31,12 +32,36 @@ namespace ATM.Controllers
             return View(person);
         }
 
-        [Gordibute]
         public ActionResult Status()
         {
             if (ControllerContext.IsChildAction)
                 return PartialView();
             return View();
+        }
+
+        public ActionResult Edit()
+        {
+            Guid id = Guid.Parse(Session["UserId"].ToString());
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Person person = db.People.Find(id);
+            person.Password = "";
+            if (person == null)
+                return HttpNotFound();
+            return View(person);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Username,Password,Firstname,Lastname,PictureFileId")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(person).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(person);
         }
 
     }
